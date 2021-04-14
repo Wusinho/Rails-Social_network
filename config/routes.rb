18d169dba0dbbfd644ea979cad61ef897cbcc77f
Heber <heberlazo@gmail.com>
2021-04-14 11:11:03 -0500

    fix bug with show latest articles

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :articles
   resources :categories
   devise_for :users
-  root 'articles#index'
+  root 'categories#index'
   put '/article/:id/vote', to: 'articles#vote', as: 'vote'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

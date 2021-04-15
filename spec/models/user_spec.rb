2c181ec8ab85b952b89d331dca79d0eee677a89b
Heber <heberlazo@gmail.com>
2021-04-15 14:43:14 -0500

    Add rspec for user

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject(:user) { User.new(username: 'Juan', email: 'juan@mail.com', password: '123456') }
+
+  describe 'Validations' do
+    it { should validate_length_of(:username).is_at_most(20) }
+
+    it { should_not allow_value('').for(:username) }
+
+    long_name = (0..20).map { ('a'..'z').to_a[rand(26)] }.join
+    it { should_not allow_value(long_name).for(:username) }
+
+    it { should allow_value(user.username).for(:username) }
+
+    it { should validate_presence_of(:email) }
+
+    email = (0...30).map { ('a'..'z').to_a[rand(26)] }.join
+    it { should_not allow_value(email).for(:email) }
+
+    it { should allow_value(user.email).for(:email) }
+
+    it { should validate_uniqueness_of(:email).case_insensitive }
+
+    it { should validate_presence_of(:password) }
+
+    password = (0...5).map { ('a'..'z').to_a[rand(26)] }.join
+    it { should_not allow_value(password).for(:password) }
+
+    it { should_not allow_value('').for(:password) }
+
+    it { should allow_value(user.password).for(:password) }
+  end
+
+  
+end

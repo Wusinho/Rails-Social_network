2e96e7ebcc0d133b83e920d534d7fe47bb130d70
Heber <heberlazo@gmail.com>
2021-04-19 11:05:09 -0500

    Add methods on helpers

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,19 @@
 module CategoriesHelper
+
+  def carousel(article)
+    if article.nil?
+        render 'default'
+    else
+        render 'default'
+    end
+  end
+
+  def categories(article)
+    if article.nil?
+        render 'default'
+    else
+        render 'default'
+    end
+  end
+
 end

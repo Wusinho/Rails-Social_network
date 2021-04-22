9b56b3f69aea6735097941baab62371729087e14
Heber <heberlazo@gmail.com>
2021-04-21 23:16:27 -0500

    Fixed TSE recomendations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :articles, :articles, only: %i[new create destroy show index] 
+  resources :articles, :articles, only: %i[new create destroy show index edit] 
   resources :categories, only: %i[show new create index]
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users
   root 'categories#index'
   put '/article/:id/vote', to: 'articles#vote', as: 'vote'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
